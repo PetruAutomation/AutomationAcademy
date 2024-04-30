e5453a35364543fb5e3a22e797cb6646f64878de
PetruAutomation <petru.pascari@orange.com>
2024-04-30 15:31:29 +0300

    Homework basic tasks

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,41 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // TASK NR. 1 Two integers are given. How many times can the second number be subtracted from the first number?
+        Scanner twoIntegers = new Scanner(System.in);
+        System.out.println("Insert 2 Integer numbers: ");
+        int firstNumber = twoIntegers.nextInt();
+        int secondNumber = twoIntegers.nextInt();
+        System.out.println(" Number " + secondNumber + " can be subtracted from " + firstNumber);
+        int counter = firstNumber / secondNumber;
+        System.out.println(counter + " times ");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+
+        // TASK NR. 2 Create a program that converts temperature from Celsius to Fahrenheit. Prompt the user to enter a temperature in Celsius,
+        // perform the conversion, and display the result.
+
+        Scanner celsius = new Scanner(System.in);
+        System.out.println("Enter temperature in Celsius: ");
+        double enterCelsius = celsius.nextDouble();
+        double fahrenheit = enterCelsius * 9 / 5 + 32;
+        System.out.println("Temperature Celsius " + enterCelsius + " To Fahrenheit is " + fahrenheit);
+
+
+        //TASK NR. 3 Write a Java program to check whether a given number is even or odd.
+
+        Scanner evenOdd = new Scanner(System.in);
+        System.out.println("Enter number to find out if even or odd ");
+        int number = evenOdd.nextInt();
+
+        if (number % 2 == 0) {
+            System.out.println("Number " + number + " is Even ");
+        } else {
+            System.out.println("Number " + number + " is Odd");
         }
     }
 }
+
+
+
